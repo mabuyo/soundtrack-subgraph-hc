4cfe7721bab093134005a11dedcf120b018a0925
Michelle Mabuyo <michelle@apollographql.com>
2023-12-07 17:15:40 -0500

    lesson 13: mutation response

---
+++ Types/AddItemsToPlaylistPayload.cs
@@ -0,0 +1,31 @@
+﻿namespace Odyssey.Liftoff;
+
+public class AddItemsToPlaylistPayload
+{
+    [GraphQLDescription("Similar to HTTP status code, represents the status of the mutation.")]
+    public int Code { get; set; }
+
+    [GraphQLDescription("Indicates whether the mutation was successful.")]
+    public bool Success { get; set; }
+
+    [GraphQLDescription("Human-readable message for the UI.")]
+    public string Message { get; set; }
+
+    [GraphQLDescription("The playlist that contains the newly added items.")]
+    public Playlist? Playlist { get; set; }
+
+    public AddItemsToPlaylistPayload(int code, bool success, string message)
+    {
+        Code = code;
+        Success = success;
+        Message = message;
+    }
+
+    public AddItemsToPlaylistPayload(int code, bool success, string message, Playlist playlist)
+    {
+        Code = code;
+        Success = success;
+        Message = message;
+        Playlist = playlist;
+    }
+}
---
+++ Types/Mutation.cs
@@ -0,0 +1,15 @@
+﻿namespace Odyssey.Liftoff;
+
+public class Mutation
+{
+    [GraphQLDescription("Add one or more items to a user's playlist.")]
+    public AddItemsToPlaylistPayload AddItemsToPlaylist()
+    {
+        return new AddItemsToPlaylistPayload(
+            200,
+            true,
+            "Successfully added  items to playlist.",
+            new Playlist("748GuzX7eACeswGoJt6hOw", "Apollo Client (web)")
+        );
+    }
+}

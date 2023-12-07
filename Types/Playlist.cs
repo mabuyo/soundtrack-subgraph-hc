604e91692134e667e9673ef1f9ada17aaaa1f287
Michelle Mabuyo <michelle@apollographql.com>
2023-12-07 12:25:48 -0500

    lesson 6: schema - playlist

---
+++ Types/Playlist.cs
@@ -0,0 +1,23 @@
+﻿namespace Odyssey.Liftoff;
+
+[GraphQLDescription("Information about a playlist owned by a Spotify user")]
+public class Playlist
+{
+    [GraphQLDescription("The Spotify ID for the playlist.")]
+    [ID]
+    public string Id { get; }
+
+    [GraphQLDescription("The name of the playlist.")]
+    public string Name { get; set; }
+
+    [GraphQLDescription(
+        "The playlist description. _Only returned for modified, verified playlists, otherwise null_."
+    )]
+    public string? Description { get; set; }
+
+    public Playlist(string id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+}

42737c4919ebdfc68ae573b658be62eba5a6468f
Michelle Mabuyo <michelle@apollographql.com>
2023-12-07 18:04:26 -0500

    lesson 14: mutation input

--- Types/Mutation.cs
+++ Types/Mutation.cs
@@ -1,15 +1,36 @@
-﻿namespace Odyssey.Liftoff;
+﻿using SpotifyWeb;
+
+namespace Odyssey.Liftoff;
 
 public class Mutation
 {
     [GraphQLDescription("Add one or more items to a user's playlist.")]
-    public AddItemsToPlaylistPayload AddItemsToPlaylist()
+    public async Task<AddItemsToPlaylistPayload> AddItemsToPlaylist(
+        AddItemsToPlaylistInput input,
+        SpotifyService spotifyService
+    )
     {
-        return new AddItemsToPlaylistPayload(
-            200,
-            true,
-            "Successfully added  items to playlist.",
-            new Playlist("748GuzX7eACeswGoJt6hOw", "Apollo Client (web)")
-        );
+        try
+        {
+            var snapshot_id = await spotifyService.AddTracksToPlaylistAsync(
+                input.PlaylistId,
+                input.Position,
+                string.Join(",", input.Uris)
+            );
+
+            var response = await spotifyService.GetPlaylistAsync(input.PlaylistId);
+            var playlist = new Playlist(response);
+
+            return new AddItemsToPlaylistPayload(
+                200,
+                true,
+                "Successfully added  items to playlist.",
+                playlist
+            );
+        }
+        catch (Exception e)
+        {
+            return new AddItemsToPlaylistPayload(500, false, e.Message);
+        }
     }
 }
